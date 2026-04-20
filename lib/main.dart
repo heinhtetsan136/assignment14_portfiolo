423d85b4540ba0f7650cc69d8e6408471fefddb6
heinhtetsan <heinhtetsan136@gmail.com>
2026-04-20 11:46:02 +0630

    portfiolo

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,68 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
       ),
-      home:Profile(),
+      home: Profile(),
     );
   }
 }
-
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    final height = size.height;
+    final width = size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+          ),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(
+                      5,
+                    ),
+                    bottomRight: Radius.circular(
+                      5,
+                    ),
+                  ),
+                ),
+
+                height: 200,
+                width: width,
+              ),
+              Positioned(
+                left: 20,
+                bottom: -50,
+                child: Container(
+                  padding: EdgeInsets.all(4),
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
